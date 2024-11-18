c17ef3f91e82edaefcbdacfe8182ea83562bb2c4
a3mmcpp <a3mmcpp@gmail.com>
2024-11-18 16:26:48 +0300

    Остановка на передаче СОХРАНЕННОГО в КЭР

--- myproj_smp/my_app_smp/views.py
+++ myproj_smp/my_app_smp/views.py
@@ -12,8 +12,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import Group
 
 
-
-def login(request):
+def login_view(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -21,22 +20,26 @@ def login(request):
 
         if user is not None:
             auth_login(request, user)
-            return redirect('home')  # Перенаправление на главную страницу
+            return redirect('home')
         else:
-            error_message = "Неверный логин или пароль."
+            error_message = "Вас нет в списке"
             return render(request, 'login.html', {'error_message': error_message})
 
     return render(request, 'login.html')
 
-# class MyprojectLogout(LogoutView):
-#     next_page  = reverse_lazy('home')
 
 def logout(request):
     auth_logout(request)  # Выход из аккаунта
     print("User logged out")  # Отладочное сообщение
-    return redirect('home')  # Перенаправление на главную страницу
+    return redirect('login')  # Перенаправление на главную страницу
+    # return render(request, 'login.html')
 
 
+# class MyprojectLogout(LogoutView):
+#     next_page  = reverse_lazy('login')
+
+
+# @login_required
 def home(request):
     # --------------
     # user_groups = request.user.groups.all()
@@ -98,57 +101,138 @@ def home(request):
         'groups': user_groups_list, # Получаем все группы
     })
 
+# @login_required
 def patient_detail(request, id):
     patient = get_object_or_404(SmpRazborTab, id=id)  # Получаем запись по ID
 
     return render(request, 'patient_detail.html', {'patient': patient})
 
+# @login_required
 def edit_patient(request, id):
     patient = get_object_or_404(SmpRazborTab, id=id)
 
-    if patient.data_poslednego_vizita_vracha_iz_protokola_osmotra_emias:
-        dni =  datetime.date.today() - patient.data_poslednego_vizita_vracha_iz_protokola_osmotra_emias
-    else: dni = 'нет даты визита врача'
+    # --------------- вычсление перес=менных по ДНЯМ!!! ------------
+
+    if patient.kakaya_data_sleduyushchego_vizita_vracha_soglasno_protokolu and patient.data_vklyucheniya_v_registr:
+        koli4_dney_ot_proshlogo_vizita_vracha = patient.kakaya_data_sleduyushchego_vizita_vracha_soglasno_protokolu - patient.data_vklyucheniya_v_registr
+    else:
+        koli4_dney_ot_proshlogo_vizita_vracha = 'нет даты визита врача'
 
     if patient.data_polucheniya_svedenij_po_vyzovam_smp_ot_kc and patient.data_naznachenogo_audioprotokola_soglasno_protokolu_v:
-        dni_zvon =  patient.data_polucheniya_svedenij_po_vyzovam_smp_ot_kc - patient.data_naznachenogo_audioprotokola_soglasno_protokolu_v
-    else: dni_zvon = 'нет звонка или  врача'
-
+        dni_kolichestvo_dnej_ot_momenta_polucheniya_dannykh_po_smp_do_sover = patient.data_polucheniya_svedenij_po_vyzovam_smp_ot_kc - patient.data_naznachenogo_audioprotokola_soglasno_protokolu_v
+    else:
+        dni_kolichestvo_dnej_ot_momenta_polucheniya_dannykh_po_smp_do_sover = 'нет звонка или  врача'
 
     if request.method == 'POST':
         # Обновляем поля, которые можно редактировать
-        # patient.polis_oms = request.POST.get('polis_oms')
-        # patient.kakova_prichina_vyzova_smp_po_rezutatam_audiokontrolya = request.POST.get('kakova_prichina_vyzova_smp_po_rezutatam_audiokontrolya')
-        # patient.vyvody_po_rezultatm_ocenki = request.POST.get('vyvody_po_rezultatm_ocenki')
-        # patient.zhaloby_opisany_v_polnom_obeme = request.POST.get('zhaloby_opisany_v_polnom_obeme')
+        # ------------------- поля для редактирования и сохранения --------
 
+        # 'fio_vracha',
+        # 'sravnenie_povoda_vyz_smp_s_prot_vracha_cpp_do_i_posle',
+        # 'dejstviya_cpp',
+        # 'kontrol_ispolneniya_naznachennykh_vrachom_cpp_rekomendacij',
+        # 'vyvod',
 
-        # --------------- поля для редактирования и сохранения --------
-        patient.byl_li_ustanovlen_bazovyj_plan_na_poslednem_vizite = request.POST.get('byl_li_ustanovlen_bazovyj_plan_na_poslednem_vizite')
-        patient.kakaya_data_sleduyushchego_vizita_vracha_soglasno_protokolu = request.POST.get('kakaya_data_sleduyushchego_vizita_vracha_soglasno_protokolu')
-        # -----------------
-        # Получаем значение из формы
-        # next_visit_date = patient.kakaya_data_sleduyushchego_vizita_vracha_soglasno_protokolu
+        patient.fio_vracha = request.POST.get('fio_vracha')
+        patient.sravnenie_povoda_vyz_smp_s_prot_vracha_cpp_do_i_posle = request.POST.get(
+            'sravnenie_povoda_vyz_smp_s_prot_vracha_cpp_do_i_posle')
+        patient.dejstviya_cpp = request.POST.get('dejstviya_cpp')
+        patient.kontrol_ispolneniya_naznachennykh_vrachom_cpp_rekomendacij = request.POST.get(
+            'kontrol_ispolneniya_naznachennykh_vrachom_cpp_rekomendacij')
+        patient.vyvod = request.POST.get('vyvod')
+
+        # -----------------------------Данные из последнего протокола врача --------------------------
+        # 'byl_li_ustanovlen_bazovyj_plan_na_poslednem_vizite',
+        # 'kakaya_data_sleduyushchego_vizita_vracha_soglasno_protokolu',
+        # 'kolichestvo_dnej_ot_proshlogo_vizita_vracha',
+        # 'otobrazheny_li_vse_zhaloby_pacienta_v_polnom_obeme',
+        # 'dinamika_sostoyaniya',
+        # 'data_naznachenogo_audioprotokola_soglasno_protokolu_v',
+
+        patient.byl_li_ustanovlen_bazovyj_plan_na_poslednem_vizite = request.POST.get(
+            'byl_li_ustanovlen_bazovyj_plan_na_poslednem_vizite')
+        patient.kakaya_data_sleduyushchego_vizita_vracha_soglasno_protokolu = request.POST.get(
+            'kakaya_data_sleduyushchego_vizita_vracha_soglasno_protokolu')
         # Проверяем, является ли значение None
         if patient.kakaya_data_sleduyushchego_vizita_vracha_soglasno_protokolu == "":
             patient.kakaya_data_sleduyushchego_vizita_vracha_soglasno_protokolu = None
         else:
             patient.kakaya_data_sleduyushchego_vizita_vracha_soglasno_protokolu = patient.kakaya_data_sleduyushchego_vizita_vracha_soglasno_protokolu
         # -----------------
-
-
-        patient.kolichestvo_dnej_ot_proshlogo_vizita_vracha= request.POST.get('kolichestvo_dnej_ot_proshlogo_vizita_vracha')
-        patient.otobrazheny_li_vse_zhaloby_pacienta_v_polnom_obeme= request.POST.get('otobrazheny_li_vse_zhaloby_pacienta_v_polnom_obeme')
-        patient.dinamika_sostoyaniya= request.POST.get('dinamika_sostoyaniya')
-        patient.data_naznachenogo_audioprotokola_soglasno_protokolu_v= request.POST.get('data_naznachenogo_audioprotokola_soglasno_protokolu_v')
+        # patient.kolichestvo_dnej_ot_proshlogo_vizita_vracha= request.POST.get('kolichestvo_dnej_ot_proshlogo_vizita_vracha')
+        # -----------------
+        patient.otobrazheny_li_vse_zhaloby_pacienta_v_polnom_obeme = request.POST.get(
+            'otobrazheny_li_vse_zhaloby_pacienta_v_polnom_obeme')
+        patient.dinamika_sostoyaniya = request.POST.get('dinamika_sostoyaniya')
+        patient.data_naznachenogo_audioprotokola_soglasno_protokolu_v = request.POST.get(
+            'data_naznachenogo_audioprotokola_soglasno_protokolu_v')
         # Проверяем, является ли значение None
         if patient.data_naznachenogo_audioprotokola_soglasno_protokolu_v == "":
             patient.data_naznachenogo_audioprotokola_soglasno_protokolu_v = None
         else:
             patient.data_naznachenogo_audioprotokola_soglasno_protokolu_v = patient.data_naznachenogo_audioprotokola_soglasno_protokolu_v
         # -----------------
-        patient.kolichestvo_dnej_ot_momenta_polucheniya_dannykh_po_smp_do_sover = request.POST.get(
-            'kolichestvo_dnej_ot_momenta_polucheniya_dannykh_po_smp_do_sover')
+        # -----------------------------Проверка заведующего --------------------------
+
+        # 'data_polucheniya_svedenij_po_vyzovam_smp_ot_kc',
+        # 'data_audioprotokola_posle_polucheniya_dannykh_o_vyzove_smp',
+        # 'kolichestvo_dnej_ot_momenta_polucheniya_dannykh_po_smp_do_sover',
+        #
+        # 'kakova_prichina_vyzova_smp_po_rezutatam_audiokontrolya',
+        # 'kakie_dejstviya_byli_predprinyaty_smp',
+        # 'tekushchee_sostoyanie_pacienta_posle_vyzova_smp',
+        # 'ostalis_li_zhaloby_posle_vyzova_smp',
+        # 'opisanie_zhalob',
+        # 'byli_li_lekarstvennye_sredstva_otovareny_po_receptu',
+        # 'pacient_prinimaet_naznachennye_lekarstvennye_sredstva',
+        # 'ehffektivna_li_naznachennaya_medikamentoznaya_terapiya',
+        # 'est_li_pobochnye_dejstviya_ot_naznachennykh_lekarstvennykh',
+        # 'pacientu_byla_ozvuchena_data_sleduyushchego_vizita',
+        # 'pacientu_byli_predostavleny_kontaktnye_nomera_cpp',
+        # 'pacient_zvonil_po_ukazannym_kontaktnym_nomera_do_vyzova_smp',
+        # 'prichina_po_kotoroj_pacient_ne_zvonil_po_ukazannym_nomeram',
+        # 'sootvetstvuet_li_naznachennyj_bazovyj_plan_sostoyaniyu_pacie',
+        # 'trebovanie_gospitalizacii_na_dannyj_moment',
+        # 'vyyavlennye_defekty_v_rabote_vracha',
+
+        patient.data_polucheniya_svedenij_po_vyzovam_smp_ot_kc = request.POST.get(
+            'data_polucheniya_svedenij_po_vyzovam_smp_ot_kc')
+        patient.data_audioprotokola_posle_polucheniya_dannykh_o_vyzove_smp = request.POST.get(
+            'data_audioprotokola_posle_polucheniya_dannykh_o_vyzove_smp')
+
+        patient.kakova_prichina_vyzova_smp_po_rezutatam_audiokontrolya = request.POST.get(
+            'kakova_prichina_vyzova_smp_po_rezutatam_audiokontrolya')
+        patient.kakie_dejstviya_byli_predprinyaty_smp = request.POST.get('kakie_dejstviya_byli_predprinyaty_smp')
+        patient.tekushchee_sostoyanie_pacienta_posle_vyzova_smp = request.POST.get(
+            'tekushchee_sostoyanie_pacienta_posle_vyzova_smp')
+        patient.ostalis_li_zhaloby_posle_vyzova_smp = request.POST.get('ostalis_li_zhaloby_posle_vyzova_smp')
+        patient.opisanie_zhalob = request.POST.get('opisanie_zhalob')
+        patient.byli_li_lekarstvennye_sredstva_otovareny_po_receptu = request.POST.get(
+            'byli_li_lekarstvennye_sredstva_otovareny_po_receptu')
+        patient.pacient_prinimaet_naznachennye_lekarstvennye_sredstva = request.POST.get(
+            'pacient_prinimaet_naznachennye_lekarstvennye_sredstva')
+        patient.ehffektivna_li_naznachennaya_medikamentoznaya_terapiya = request.POST.get(
+            'ehffektivna_li_naznachennaya_medikamentoznaya_terapiya')
+        patient.est_li_pobochnye_dejstviya_ot_naznachennykh_lekarstvennykh = request.POST.get(
+            'est_li_pobochnye_dejstviya_ot_naznachennykh_lekarstvennykh')
+        patient.pacientu_byla_ozvuchena_data_sleduyushchego_vizita = request.POST.get(
+            'pacientu_byla_ozvuchena_data_sleduyushchego_vizita')
+        patient.pacientu_byli_predostavleny_kontaktnye_nomera_cpp = request.POST.get(
+            'pacientu_byli_predostavleny_kontaktnye_nomera_cpp')
+        patient.pacient_zvonil_po_ukazannym_kontaktnym_nomera_do_vyzova_smp = request.POST.get(
+            'pacient_zvonil_po_ukazannym_kontaktnym_nomera_do_vyzova_smp')
+        patient.prichina_po_kotoroj_pacient_ne_zvonil_po_ukazannym_nomeram = request.POST.get(
+            'prichina_po_kotoroj_pacient_ne_zvonil_po_ukazannym_nomeram')
+        patient.sootvetstvuet_li_naznachennyj_bazovyj_plan_sostoyaniyu_pacie = request.POST.get(
+            'sootvetstvuet_li_naznachennyj_bazovyj_plan_sostoyaniyu_pacie')
+        patient.trebovanie_gospitalizacii_na_dannyj_moment = request.POST.get(
+            'trebovanie_gospitalizacii_na_dannyj_moment')
+        patient.vyyavlennye_defekty_v_rabote_vracha = request.POST.get('vyyavlennye_defekty_v_rabote_vracha')
+
+        # -----------------------
+        # patient.kolichestvo_dnej_ot_momenta_polucheniya_dannykh_po_smp_do_sover = request.POST.get(
+        #     'kolichestvo_dnej_ot_momenta_polucheniya_dannykh_po_smp_do_sover')
+        # -----------------------
 
         patient.save()  # Сохраняем изменения
         return redirect('home',
@@ -156,8 +240,8 @@ def edit_patient(request, id):
                         )  # Перенаправляем на главную страницу
 
     return render(request, 'edit_pacient_short.html', {'patient': patient,
-                                                       'dni': dni,
-                                                       'dni_zvon':dni_zvon,
+                                                       'koli4_dney_ot_proshlogo_vizita_vracha': koli4_dney_ot_proshlogo_vizita_vracha,
+                                                       'dni_kolichestvo_dnej_ot_momenta_polucheniya_dannykh_po_smp_do_sover': dni_kolichestvo_dnej_ot_momenta_polucheniya_dannykh_po_smp_do_sover,
                                                        })
 
 def edit_ker(request, id):
@@ -219,9 +303,10 @@ def edit_ker(request, id):
 
 
 def send_to_ker(request, id):
-# def send_to_ker(id):
     record = get_object_or_404(SmpRazborTab, id=id)
     record.ok_vps = "Передано в КЭР"  # Изменяем состояние поля
     record.save()  # Сохраняем изменения
 
     return redirect('home')  # Перенаправляем на нужный URL
+
+
--- myproj_smp/myproj_smp/urls.py
+++ myproj_smp/myproj_smp/urls.py
@@ -17,12 +17,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LogoutView
+from my_app_smp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('', include('my_app_smp.url_smp')),  # Подключаем маршруты приложения
-    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),   ###### 3
-    path('accounts/logout/', auth_views.LogoutView.as_view(), name=' home') ###### 3
+    # path('accounts/login/', auth_views.LoginView.as_view(), name='login'),   ###### 3
+    # path('accounts/logout/', auth_views.LogoutView.as_view(), name='login') ###### 3
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('login/', views.login_view, name='login'),  # Предполагается, что у вас есть представление для входа
 
 ]
