d76d7fed9ddd045b5de12e2423c3ee2eeb0f41dd
cheefdepat <cheff.department@gmail.com>
2024-11-29 17:03:08 +0300

    кис - фильтры из формы

--- myproj_smp/app_kis_long/forms_kis.py
+++ myproj_smp/app_kis_long/forms_kis.py
@@ -23,5 +23,24 @@ class KisLongFormСhoiceMedKriter(forms.Form):
 
 
 class KisLongFilterForm(forms.Form):
-    fio_pacienta = forms.CharField(label='ФИО', required=False)
-    otdelenie = forms.CharField(label='Отделение', required=False)
+    fio_pacienta = forms.CharField(label='ФИО',
+                                   required=False,
+                                   widget=forms.TextInput(
+                                       attrs={'style': 'width: 350px; background-color: #87CEEB;'})
+                                   )
+    otdelenie = forms.CharField(label='Отделение',
+                                required=False,
+                                widget=forms.TextInput(
+                                    attrs={'style': 'width: 150px; background-color: #C6F4D6;'})
+                                 )
+
+    kojko_dni_min = forms.IntegerField(label='Количество дней (мин.)',
+                                       required=False,
+                                       # initial=1,
+                                       widget=forms.NumberInput(
+                                              attrs={'style': 'width: 50px; background-color: #C6F4D6;'}))
+    kojko_dni_max = forms.IntegerField(label='Количество дней (макс.)',
+                                       required=False,
+                                       # initial=3000,
+                                       widget=forms.NumberInput(
+                                               attrs={'style': 'width: 50px; background-color: #C6F4D6;'}))
